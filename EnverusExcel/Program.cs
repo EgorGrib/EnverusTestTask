6a870fcdd3d11f5681e3f1626f9b274ce65169b3
unknown <egor111orexov@gmail.com>
2023-08-23 13:23:42 +0200

    initial

---
+++ EnverusExcel/Program.cs
@@ -0,0 +1,70 @@
+﻿using HtmlAgilityPack;
+using Microsoft.Extensions.Configuration;
+
+var basePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\"));
+
+IConfiguration config = new ConfigurationBuilder()
+    .SetBasePath(basePath)
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var websiteUrl = config["targetWebsite"];
+var targetFile = config["targetFile"];
+var outPath = config["outPath"];
+
+using var client = new HttpClient();
+try
+{
+    if (websiteUrl is null) throw new ArgumentNullException("targetWebsite");
+    if (targetFile is null) throw new ArgumentNullException("targetFile");
+    if (outPath is null) throw new ArgumentNullException("outPath");
+
+    client.DefaultRequestHeaders.Add("User-Agent", 
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36");
+    client.DefaultRequestHeaders.Add("Accept", 
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+    client.DefaultRequestHeaders.Add("Accept-Language", "en-US,en;q=0.5");
+    client.DefaultRequestHeaders.Add("Accept-Encoding", "deflate,br");
+    
+    var content = await client.GetStringAsync(websiteUrl);
+    var doc = new HtmlDocument();
+    doc.LoadHtml(content);
+
+    HtmlNode linkNode = doc.DocumentNode.SelectSingleNode($"//a[contains(text(), '{targetFile}')]");
+
+    if (linkNode != null)
+    {
+        var href = linkNode.GetAttributeValue("href", "");
+
+        var uri = new Uri(websiteUrl);
+        var rootDomain = uri.Scheme + "://" + uri.Host;
+
+        var link = rootDomain + href;
+        var savePath = outPath + @"/" + $"{targetFile}.xlsx";
+        
+        HttpResponseMessage response = await client.GetAsync(link);
+
+        if (response.IsSuccessStatusCode)
+        {
+            await using (var contentStream = await response.Content.ReadAsStreamAsync())
+            await using (var fileStream = File.Create(savePath))
+            {
+                await contentStream.CopyToAsync(fileStream);
+            }
+
+            Console.WriteLine("File downloaded and saved successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Failed to download the file. Status code: " + response.StatusCode);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Link not found.");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Error: {e.Message}");
+}
